82bdb955ea5ab5d3d4fdfe48a4b69abac056696d
Anson Yu Wang <opensource@undozen.com>
2015-07-01 09:08:20 +0800

    use intercom to sync between tabs & store is property now

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
 'use strict';
 
 var Bacon = require('baconjs');
+var _ = require('lodash');
+var intercom;
+if (require('is-browser')) {
+    require('./intercom');
+    intercom = Intercom.getInstance();
+}
 
 var busCache = {};
 exports.bus = function () {
+    /*
+     * bus('register')
+     * bus('register/loginName')
+     * bus('register/loginName/value')
+     *
+     */
     var busName = Array.prototype.slice.call(arguments).join('/');
     var bus;
     if ((bus = busCache[busName])) {
         return bus;
     }
     bus = busCache[busName] = new Bacon.Bus();
+    if (busName.indexOf('store/') === 0) {
+        var property = bus.toProperty();
+        _.each('push end error plug'.split(' '), function (method) {
+            property[method] = bus[method].bind(bus);
+        });
+        bus = property;
+    }
+    bus.onValue(function (value) {
+        bus.currentValue = value;
+        if (intercom && value && value.syncAllTab === true && !value.syncedFromOtherTab) {
+            intercom.emit(busName, _.assign({}, value, {syncedFromOtherTab: true}));
+        }
+    });
+    if (intercom) {
+        intercom.on(busName, function (value) {
+            console.log(arguments);
+            bus.push(value);
+        });
+    }
     return bus;
 };
 exports.action = exports.bus.bind(null, 'action');
 exports.async = exports.bus.bind(null, 'async');
 exports.store = exports.bus.bind(null, 'store');
-exports.property = function () {
-    var store = exports.store.apply(null, arguments);
-    return store._property || (store._property = store.toProperty());
-};
